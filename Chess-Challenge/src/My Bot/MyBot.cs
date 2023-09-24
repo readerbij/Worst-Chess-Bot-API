4c1934a1f4a0dbe4b586d8468f0947168b72c651
readerbij <studentbjt@gmail.com>
2023-09-24 08:48:17 -0700

    Update MyBot.cs

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -7,7 +7,7 @@ public class MyBot : IChessBot
 {
     
     
-    int[] pieceValues = { 0, 100, 300, 300, 500, 900, 10000 };
+    
 
     public Move Think(Board board, Timer timer)
     {
@@ -16,27 +16,7 @@ public class MyBot : IChessBot
         // Pick a random move to play if nothing better is found
         Random rng = new();
         Move moveToPlay = allMoves[rng.Next(allMoves.Length)];
-        int lowestValueCapture = 100000;
-
-        foreach (Move move in allMoves)
-        {
-            
-            // Find lowest value capture
-            Piece capturedPiece = board.GetPiece(move.TargetSquare);
-            int capturedPieceValue = pieceValues[(int)capturedPiece.PieceType];
-            // Always play a move if it doesn't capture a piece
-            if (capturedPieceValue == 0)
-            {
-                moveToPlay = move;
-                break;
-            }
-
-            if (capturedPieceValue < lowestValueCapture)
-            {
-                moveToPlay = move;
-                lowestValueCapture = capturedPieceValue;
-            }
-        }
+        
 
         return moveToPlay;
     }
